364b079cece6679636b47ae1cf672d612280e29f
青石千梦 <yougewangyi@126.com>
2012-11-02 13:15:34 +0800

    优化显示流程；加入注释
    =-=话说每次都是把代码写完了才加注释是怎么一回事……

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+	/*	
+		Ainou Surface Cutting[surface-cutting]
+		via http://thousandmoe.github.com/surface-cutting
+		Copyright 2012 青石千梦[thousandmoe]
+		yougewangyi@126.com
+		Released under the GPL Licenses.
+	*/
+	@error_reporting(0);	//屏蔽错误报告
+	header('Content-Type: text/html; charset=utf-8');	//用于治疗某些国外空间乱码的坏毛病。PH(php & html)合璧，疗效快=w=
+	set_time_limit(0);	//防止传大图的时候到达限制时间而退出，话说到达限制时间那是有多大的图啊=v=
+	
+	//图片处理流程
+	if(isset($_FILES["file"])){
+		$base64=base64_encode(file_get_contents($_FILES["file"]["tmp_name"]));	//base64编码上传的文件
+		$src='data:'.$_FILES["file"]["type"].';base64,'.$base64;	//输出类似于data:image/png;base64,blahblah这样的格式=w=这样才能做到不保存图片~
+	}else{
+		$src='surface0.png';
+	}
+?>
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -41,14 +61,6 @@
 		<form action="<?=$_SERVER['PHP_SELF'];?>" method="post" enctype="multipart/form-data" id="control" class="des">
 		上传：<input type="file" name="file" id="file" size="20" /><input type="submit" value="提交" /><br />
 		坐标：<input name="ukagaka" id="ukagaka" size="20" type="text"></form>
-	<?php
-		if(isset($_FILES["file"])){
-			$base64=base64_encode(file_get_contents($_FILES["file"]["tmp_name"]));
-			$src='data:'.$_FILES["file"]["type"].';base64,'.$base64;
-		}else{
-			$src='surface0.png';
-		}
-	?>
 		<script src="v.js"></script>
 		<script language="javascript">
 			v.load('vcrop.js', function(){
