84bfe6f2c2b07b5b5ed8850f0189c7b3bb8f9b16
青石千梦 <yougewangyi@126.com>
2012-11-02 12:44:50 +0800

    修改版权说明；修改排版

--- index.php
+++ index.php
@@ -5,15 +5,15 @@
 	</head>
 	<body>
 	<div style="padding:20px 0 0 20px;" id="control">
-		<form action="<?=$_SERVER['PHP_SELF'];?>" method="post" enctype="multipart/form-data">
-		上传：<input type="file" name="file" id="file" /><input type="submit" value="提交" /><br />
 		请浏览您的shell图片后提交，您的shell图片将出现在下方。<br />
 		请使用鼠标拖动选取对应的区域，然后复制下方的坐标。<br />
 		本程序不会保存您的shell图片，请放心使用。<br />
 		如果页面浏览有困扰，请使用最新版Firefox或者Google Chrome浏览器。<br />
-		默认图片取自「葡萄的葡萄架子」的人格「炸酱一只」，未经授权请勿使用。<br />
+		默认图片取自「葡萄的葡萄架子」的人格「炸酱一只」，未经授权请勿作它用。<br />
 		项目主页：<a href="http://thousandmoe.github.com/surface-cutting">surface-cutting</a><br />
-		坐标：<input name="ukagaka" id="ukagaka" size="20" type="text">
+		<form action="<?=$_SERVER['PHP_SELF'];?>" method="post" enctype="multipart/form-data">
+		上传：<input type="file" name="file" id="file" size="20" /><input type="submit" value="提交" /><br />
+		坐标：<input name="ukagaka" id="ukagaka" size="20" type="text"></form>
 	</div>
 	<?php
 		if(isset($_FILES["file"])){
